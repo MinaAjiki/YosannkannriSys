95ee51ae15def2be7edff442756e5eb15af3670b
MinaAjiki <mina_ajiki@ic-group.co.jp>
2021-11-01 13:29:13 +0900

    費用マスタ変更

--- 予算管理システム(仮)/My Project/Resources.Designer.vb
+++ 予算管理システム(仮)/My Project/Resources.Designer.vb
@@ -69,5 +69,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        '''</summary>
+        Friend ReadOnly Property Search() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Search", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
