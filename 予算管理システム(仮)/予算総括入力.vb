f756038375bbe7bdeb51f46d8e19c2892bfee854
MinaAjiki <mina_ajiki@ic-group.co.jp>
2021-11-01 15:44:41 +0900

    追加

---
+++ 予算管理システム(仮)/予算総括入力.vb
@@ -0,0 +1,26 @@
+﻿Public Class 予算総括入力
+    Private Sub 予算総括入力_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_MouseLeave(sender As Object, e As EventArgs) Handles Entry.MouseLeave
+        Entry.ImageIndex = 3
+    End Sub
+
+    Private Sub Button4_MouseEnter(sender As Object, e As EventArgs) Handles Entry.MouseEnter
+        Entry.Cursor = Cursors.Hand
+        Entry.ImageIndex = 4
+    End Sub
+
+    Private Sub Button4_MouseDown(sender As Object, e As MouseEventArgs) Handles Entry.MouseDown
+        Entry.ImageIndex = 5
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Entry.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        業者一覧.Show()
+    End Sub
+End Class
