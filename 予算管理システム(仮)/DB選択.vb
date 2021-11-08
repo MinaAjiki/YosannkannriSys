a0c9ebc425ef873bcbc50fae73f9af1d89fe5636
MinaAjiki <mina_ajiki@ic-group.co.jp>
2021-11-08 17:07:28 +0900

    コード追加

--- 予算管理システム(仮)/DB選択.vb
+++ 予算管理システム(仮)/DB選択.vb
@@ -1,10 +1,28 @@
-﻿Public Class DB選択
+﻿Imports System.Data.SqlClient
+Imports System.Deployment.Application
+Imports System.IO.DirectoryInfo
+Imports System.ComponentModel
+Imports System.Deployment.Application.ApplicationDeployment
+Imports System.Windows.Forms.Form
+Public Class DB選択
+    Dim systmcnnctn As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & Application.StartupPath & "\予算管理システム(system_sql).mdf;Integrated Security=True")
+    Dim systemsql As New SqlCommand
+
     Private Sub DB選択_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         FileList.Rows(0).Height = 20
-        FileList(1, 1) = "DD0000テスト工事"
-        FileList(1, 2) = "D:\現場データ\DD0000テスト工事.mdf"
-        FileList(2, 1) = "DH0000テスト舗装"
-        FileList(2, 2) = "C:\Users\206029\デスクトップ\現場データ\DH0000テスト舗装.mdf"
+
+        systmcnnctn.Open()
+        systemsql.Connection = systmcnnctn
+        Dim datacount As Integer = 1
+        systemsql.CommandText = "SELECT TOP 5 * FROM userfiles ORDER BY updatedate ASC"
+        Dim filereader As SqlDataReader = systemsql.ExecuteReader
+        While filereader.Read
+            FileList(datacount, 1) = filereader.Item("filename")
+            FileList(datacount, 2) = filereader.Item("filepath")
+            datacount += 1
+        End While
+        filereader.Close()
+
     End Sub
 
     Private Sub Open_MouseEnter(sender As Object, e As EventArgs) Handles Open.MouseEnter
@@ -41,4 +59,49 @@
         OtherFile.ImageIndex = 0
     End Sub
 
+    Private Sub OtherFile_Click(sender As Object, e As EventArgs) Handles OtherFile.Click
+        OtherFileOpenDialog.ShowDialog()
+    End Sub
+
+    Private Sub OtherFileOpenDialog_FileOk(sender As Object, e As CancelEventArgs) Handles OtherFileOpenDialog.FileOk
+
+        Dim openfilename As String = OtherFileOpenDialog.FileName
+
+        If openfilename.Contains(".mdf") = False Then
+            MsgBox("mdfファイルを選択してください。", MsgBoxStyle.Exclamation, "エラー")
+            Exit Sub
+
+        Else
+
+            ホーム.Connection.Close()
+            ホーム.Connection.Dispose()
+
+            ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & openfilename & ";Integrated Security=True"
+            ホーム.Connection.Open()
+            ホーム.SQL.Connection = ホーム.Connection
+
+            Dim lastindex As Integer = openfilename.LastIndexOf("\")
+            Dim length As Integer = openfilename.Length
+
+            Dim filename As String = openfilename.Remove(0, lastindex + 1)
+
+            systemsql.CommandText = "SELECT Count(filename) FROM userfiles WHERE filename=" & filename
+            Dim filecount As Integer = systemsql.ExecuteScalar
+
+            If filecount >= 1 Then
+                systemsql.CommandText = "UPDATE userfiles SET updatedate=" & Today & " WHERE filename=" & filename
+                systemsql.ExecuteNonQuery()
+            Else
+                systemsql.CommandText = "INSERT INTO userfiles (filename,filepath,updatedate) VALLUES (@filename,@failepath,@updatedate)"
+                systemsql.Parameters.Add(New SqlParameter("@filename", SqlDbType.NVarChar))
+                systemsql.Parameters.Add(New SqlParameter("@filepath", SqlDbType.NVarChar))
+                systemsql.Parameters.Add(New SqlParameter("@updatedate", SqlDbType.Date))
+                systemsql.Parameters("@filename").Value = filename
+                systemsql.Parameters("@filepath").Value = openfilename
+                systemsql.Parameters("@updatedate").Value = Today
+                systemsql.ExecuteNonQuery()
+            End If
+        End If
+
+    End Sub
 End Class
--- 予算管理システム(仮)/予算選択.vb
+++ 予算管理システム(仮)/予算選択.vb
@@ -1,12 +1,64 @@
-﻿Public Class 予算選択
+﻿Imports System.Data.SqlClient
+Imports System.Deployment.Application
+Imports System.IO.DirectoryInfo
+Imports System.ComponentModel
+Imports System.Deployment.Application.ApplicationDeployment
+Imports System.Windows.Forms.Form
+Public Class 予算選択
     Private Sub OK_Click(sender As Object, e As EventArgs) Handles OK.Click
+        ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & FilePath.Text & ";Integrated Security=True"
+        ホーム.Connection.Open()
+        ホーム.SQL.Connection = ホーム.Connection
+
         ホーム.Enabled = True
         Me.Close()
     End Sub
 
     Private Sub 予算選択_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Project.Value = "DD0000 テスト工事"
-        FilePath.Value = "C:\Users\206029\デスクトップ\現場データ\DH0000テスト舗装.mdf"
+
+        Dim path As String = ""
+        ホーム.systemsql.CommandText = "SELECT MAX(updatedate),filepath FROM userfiles GROUP BY filepath"
+        Dim filereader As SqlDataReader = ホーム.systemsql.ExecuteReader
+        While filereader.Read
+            path = filereader.Item("filepath")
+        End While
+        filereader.Close()
+
+        If path <> "" Then
+            Dim latestcnnctn As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & path & ";Integrated Security=True")
+            Dim latestsql As New SqlCommand
+            latestcnnctn.Open()
+            latestsql.Connection = latestcnnctn
+            Dim prjct_name As String = ""
+            Dim prjct_code As String = ""
+            Dim cntrct_no As Integer = 0
+            latestsql.CommandText = "select max(cntrct_no),prjct_code,prjct_name from budgetsummary"
+            Dim latestreader As SqlDataReader = latestsql.ExecuteReader
+            While latestreader.Read
+                prjct_code = latestreader.Item("prjct_code")
+                prjct_name = latestreader.Item("prjct_name")
+                cntrct_no = latestreader.Item("contract_no")
+            End While
+            latestreader.Close()
+
+            Contract_NoList.Items.Add("当初")
+
+            For contrctno As Integer = 1 To cntrct_no
+                Contract_NoList.Items.Add("第" & contrctno & "回変更")
+            Next
+
+            Contract_NoList.Items.Add("変更予算作成")
+
+            Project.Value = prjct_code & " " & prjct_name
+            FilePath.Value = path
+            Contract_NoList.SelectedItem = cntrct_no
+
+        Else
+
+            ホーム.Enabled = True
+            Me.Close()
+        End If
+
     End Sub
     Private Sub OK_MouseEnter(sender As Object, e As EventArgs) Handles OK.MouseEnter
         OK.Cursor = Cursors.Hand
