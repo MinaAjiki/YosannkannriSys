578cb0041a6d033fff2760f59abaa1849396ae94
217003 <kazumasa_suzuki@ic-group.co.jp>
2021-11-15 16:58:07 +0900

    #1

--- 予算管理システム(仮)/協力業者入力.vb
+++ 予算管理システム(仮)/協力業者入力.vb
@@ -27,7 +27,7 @@ Public Class 協力業者入力
             CoopVendorList(datacount, 2) = Coopreader.Item("outsrcr_name")
             CoopVendorList(datacount, 3) = Coopreader.Item("outsrcr_trm_s")
             CoopVendorList(datacount, 4) = Coopreader.Item("outsrcr_trm_e")
-            Dim ecn As String = Coopreader.Item("e-cntrct")
+            Dim ecn As String = Coopreader.Item("ecntrct")
             If ecn = "true" Then
                 CoopVendorList.SetCellCheck(datacount, 6, CheckEnum.Checked)
             Else
@@ -70,16 +70,69 @@ Public Class 協力業者入力
         Entry.ImageIndex = 5
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Entry.Click
-        Me.Close()
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles VendorSelect.Click
         業者一覧.Show()
     End Sub
 
     Private Sub CoopVendorList_CellChanged(sender As Object, e As RowColEventArgs) Handles CoopVendorList.AfterEdit
-        Dim c As Integer = e.Row
-        CoopVendorList.SetCellImage(c, 8, Image.FromFile(Application.StartupPath & "\Edit_source.png"))
+        Dim SetImageRow As Integer = e.Row
+        CoopVendorList.SetCellImage(SetImageRow, 8, Image.FromFile(Application.StartupPath & "\Edit_source.png"))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Entry.Click
+        Dim RowIndex As Integer = CoopVendorList.Rows.Count - 1
+        For Vendorloop As Integer = 1 To RowIndex - 1
+            Dim CoopID As CellRange = CoopVendorList.GetCellRange(Vendorloop, 1)
+            Dim CoopName As CellRange = CoopVendorList.GetCellRange(Vendorloop, 2)
+            Dim Cooptrms As CellRange = CoopVendorList.GetCellRange(Vendorloop, 3)
+            Dim Cooptrme As CellRange = CoopVendorList.GetCellRange(Vendorloop, 4)
+            'Dim Coopordr As CellRange = CoopVendorList.GetCellRange(Vendorloop, 5)
+            Dim Coopcntrct As CellRange = CoopVendorList.GetCellRange(Vendorloop, 6)
+            Dim CoopDeleteF As CellRange = CoopVendorList.GetCellRange(Vendorloop, 7)
+            ホーム.SystemSql.CommandText = ""
+            ホーム.SystemSql.Parameters.Clear()
+            ホーム.SystemSql.CommandText = "Select count(*) from Outsourcers where outsrcr_code = " & CoopID.Data
+            Dim CoopIDcount As Integer = ホーム.SystemSql.ExecuteScalar
+            If Cooptrme.Data = Nothing Then
+                MsgBox("実施工期を入力してください。", MsgBoxStyle.OkOnly, "エラー")
+                Exit Sub
+            End If
+            If CoopIDcount = 0 Then
+                ホーム.SystemSql.CommandText = ""
+                ホーム.SystemSql.Parameters.Clear()
+                ホーム.SystemSql.CommandText = "INSERT INTO Outsourcers (outsrcr_code,outsrcr_name,outsrcr_trm_s,outsrcr_trm_e,ordrfrm,ecntrct) VALUES (@outsrcr_code,@outsrcr_name,@outsrcr_trm_s,@outsrcr_trm_e,@ordrfrm,@ecntrct)"
+            Else
+                ホーム.SystemSql.CommandText = ""
+                ホーム.SystemSql.Parameters.Clear()
+                ホーム.SystemSql.CommandText = "UPDATE Outsourcers SET outsrcr_name=@outsrcr_name,outsrcr_trm_s=@outsrcr_trm_s,outsrcr_trm_e=@outsrcr_trm_e,ordrfrm=@ordrfrm,ecntrct=@ecntrct where outsrcr_code=@outsrcr_code"
+            End If
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@outsrcr_code", SqlDbType.Int))
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@outsrcr_name", SqlDbType.NVarChar))
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@outsrcr_trm_s", SqlDbType.DateTime))
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@outsrcr_trm_e", SqlDbType.DateTime))
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@ordrfrm", SqlDbType.SmallInt))
+            ホーム.SystemSql.Parameters.Add(New SqlParameter("@ecntrct", SqlDbType.NVarChar))
+            ホーム.SystemSql.Parameters("@outsrcr_code").Value = CoopID.Data
+            ホーム.SystemSql.Parameters("@outsrcr_name").Value = CoopName.Data
+            ホーム.SystemSql.Parameters("@outsrcr_trm_s").Value = Cooptrms.Data
+            ホーム.SystemSql.Parameters("@outsrcr_trm_e").Value = Cooptrme.Data
+            ホーム.SystemSql.Parameters("@ordrfrm").Value = 1
+            If Coopcntrct.Data = True Then
+                ホーム.SystemSql.Parameters("@ecntrct").Value = "true"
+            Else
+                ホーム.SystemSql.Parameters("@ecntrct").Value = "false"
+            End If
+
+            If CoopDeleteF.Data = True Then
+                ホーム.Sql.Parameters.Clear()
+                ホーム.Sql.CommandText = "DELETE FROM Outsourcers WHERE outsrcr_code=" & CoopID.Data
+                ホーム.Sql.ExecuteNonQuery()
+            End If
+
+
+            ホーム.SystemSql.ExecuteNonQuery()
+        Next
+
+        Me.Close()
     End Sub
 End Class
