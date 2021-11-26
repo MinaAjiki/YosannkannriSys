f5c7508e30131cf0f3cb82d28a647332e351cbb0
217003 <kazumasa_suzuki@ic-group.co.jp>
2021-11-26 14:04:40 +0900

    #1

--- 予算管理システム(仮)/注文書番号入力.vb
+++ 予算管理システム(仮)/注文書番号入力.vb
@@ -20,17 +20,28 @@ Public Class 注文書番号入力
     End Sub
 
     Private Sub 注文書番号入力_Load(sender As Object, e As EventArgs) Handles Me.Load
-        ホーム.Connection.Close()
-        ホーム.Connection.Dispose()
-        'ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & ホーム.UserDataPath & "" & ホーム.UserDataName & ";Integrated Security=True"
-        ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\217003\source\repos\MinaAjiki\YosankanriSys\予算管理システム(仮)\現場データ.mdf;Integrated Security=True"
-        ホーム.Connection.Open()
-        ホーム.SystemSql.Connection = ホーム.Connection
+        'ホーム.Connection.Close()
+        'ホーム.Connection.Dispose()
+        ''ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & ホーム.UserDataPath & "" & ホーム.UserDataName & ";Integrated Security=True"
+        'ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\217003\source\repos\MinaAjiki\YosankanriSys\予算管理システム(仮)\現場データ.mdf;Integrated Security=True"
+        'ホーム.Connection.Open()
+        'ホーム.SystemSql.Connection = ホーム.Connection
+
+        '行数取得のため協力業者数カウント
+        ホーム.Sql.Parameters.Clear()
+        ホーム.Sql.CommandText = "SELECT count(outsrcr_id) FROM outsourcers"
+        Dim Outsrcrcount As Integer = ホーム.Sql.ExecuteScalar
+
+        If Outsrcrcount = 0 Then
+            MsgBox("外注業者を登録してください。", MsgBoxStyle.OkOnly, "エラー")
+            Exit Sub
+        End If
 
         '協力業者テーブルからデータを取得
         Dim datacount As Integer = 1
-        ホーム.SystemSql.CommandText = "SELECT * FROM outsourcers"
-        Dim Coopreader As SqlDataReader = ホーム.SystemSql.ExecuteReader
+        ホーム.Sql.CommandText = "SELECT * FROM outsourcers"
+        Dim Coopreader As SqlDataReader = ホーム.Sql.ExecuteReader
+
         While Coopreader.Read
             OrderNoList(datacount, 0) = Coopreader.Item("outsrcr_id")
             OrderNoList(datacount, 2) = Coopreader.Item("outsrcr_code")
@@ -53,17 +64,12 @@ Public Class 注文書番号入力
 
         datacount = 1
 
-        '行数取得のため協力業者数カウント
-        ホーム.SystemSql.Parameters.Clear()
-        ホーム.SystemSql.CommandText = "SELECT count(outsrcr_id) FROM outsourcers"
-        Dim Outsrcrcount As Integer = ホーム.SystemSql.ExecuteScalar
-
         '合計注文金額を計算
         For Vendorloop As Integer = 1 To Outsrcrcount
             '外注業者IDで明細書テーブルから明細書IDを取得
-            ホーム.SystemSql.Parameters.Clear()
-            ホーム.SystemSql.CommandText = "SELECT dtl_id FROM details WHERE outsrcr_id = " & OrderNoList(datacount, 0)
-            Dim DtlID As SqlDataReader = ホーム.SystemSql.ExecuteReader
+            ホーム.Sql.Parameters.Clear()
+            ホーム.Sql.CommandText = "SELECT dtl_id FROM details WHERE outsrcr_id = " & OrderNoList(datacount, 0)
+            Dim DtlID As SqlDataReader = ホーム.Sql.ExecuteReader
             '明細書IDごとに計算し合計して出力
             Dim Total As New Integer
             While DtlID.Read
@@ -80,9 +86,9 @@ Public Class 注文書番号入力
         datacount = 1
 
         '注文書テーブルから注文番号を取得
-        ホーム.SystemSql.Parameters.Clear()
-        ホーム.SystemSql.CommandText = "SELECT * FROM purchase_orders"
-        Dim OrderNoreader As SqlDataReader = ホーム.SystemSql.ExecuteReader
+        ホーム.Sql.Parameters.Clear()
+        ホーム.Sql.CommandText = "SELECT * FROM purchase_orders"
+        Dim OrderNoreader As SqlDataReader = ホーム.Sql.ExecuteReader
         While OrderNoreader.Read
             If OrderNoreader.Item("outsrcr_id") = OrderNoList(datacount, 0) Then
                 If OrderNoreader.Item("no") = Nothing Then
@@ -104,9 +110,9 @@ Public Class 注文書番号入力
     End Sub
 
     Private Sub Entry_Click(sender As Object, e As EventArgs) Handles Entry.Click
-        ホーム.SystemSql.Parameters.Clear()
-        ホーム.SystemSql.CommandText = "SELECT count(outsrcr_id) FROM outsourcers"
-        Dim Outsrcrcount As Integer = ホーム.SystemSql.ExecuteScalar
+        ホーム.Sql.Parameters.Clear()
+        ホーム.Sql.CommandText = "SELECT count(outsrcr_id) FROM outsourcers"
+        Dim Outsrcrcount As Integer = ホーム.Sql.ExecuteScalar
         For Vendorloop As Integer = 1 To Outsrcrcount
             Dim CoopID As CellRange = OrderNoList.GetCellRange(Vendorloop, 0)
             Dim OrderNo As CellRange = OrderNoList.GetCellRange(Vendorloop, 1)
@@ -114,23 +120,23 @@ Public Class 注文書番号入力
                 MsgBox("注文番号を入力してください。", MsgBoxStyle.OkOnly, "エラー")
                 Exit Sub
             End If
-            ホーム.SystemSql.Parameters.Clear()
-            ホーム.SystemSql.CommandText = "Select count(outsrcr_id) from purchase_orders where outsrcr_id =" & CoopID.Data
-            Dim CoopIDcount As Integer = ホーム.SystemSql.ExecuteScalar
+            ホーム.Sql.Parameters.Clear()
+            ホーム.Sql.CommandText = "Select count(outsrcr_id) from purchase_orders where outsrcr_id =" & CoopID.Data
+            Dim CoopIDcount As Integer = ホーム.Sql.ExecuteScalar
             If CoopIDcount = 0 Then
-                ホーム.SystemSql.CommandText = ""
-                ホーム.SystemSql.Parameters.Clear()
-                ホーム.SystemSql.CommandText = "INSERT INTO purchase_orders (outsrcr_id,no) VALUES (@outsrcr_id,@no)"
+                ホーム.Sql.CommandText = ""
+                ホーム.Sql.Parameters.Clear()
+                ホーム.Sql.CommandText = "INSERT INTO purchase_orders (outsrcr_id,no) VALUES (@outsrcr_id,@no)"
             Else
-                ホーム.SystemSql.CommandText = ""
-                ホーム.SystemSql.Parameters.Clear()
-                ホーム.SystemSql.CommandText = "UPDATE purchase_orders SET no=@no where outsrcr_id=@outsrcr_id"
+                ホーム.Sql.CommandText = ""
+                ホーム.Sql.Parameters.Clear()
+                ホーム.Sql.CommandText = "UPDATE purchase_orders SET no=@no where outsrcr_id=@outsrcr_id"
             End If
-            ホーム.SystemSql.Parameters.Add(New SqlParameter("@outsrcr_id", SqlDbType.Int))
-            ホーム.SystemSql.Parameters.Add(New SqlParameter("@no", SqlDbType.NVarChar))
-            ホーム.SystemSql.Parameters("@outsrcr_id").Value = CoopID.Data
-            ホーム.SystemSql.Parameters("@no").Value = OrderNo.Data
-            ホーム.SystemSql.ExecuteNonQuery()
+            ホーム.Sql.Parameters.Add(New SqlParameter("@outsrcr_id", SqlDbType.Int))
+            ホーム.Sql.Parameters.Add(New SqlParameter("@no", SqlDbType.NVarChar))
+            ホーム.Sql.Parameters("@outsrcr_id").Value = CoopID.Data
+            ホーム.Sql.Parameters("@no").Value = OrderNo.Data
+            ホーム.Sql.ExecuteNonQuery()
         Next
         Me.Close()
     End Sub
