8e63aef34bfbf402ee0fc0ca01a6d6e9bcfbb5e2
MinaAjiki <mina_ajiki@ic-group.co.jp>
2021-12-03 17:08:25 +0900

    追加

--- 予算管理システム(仮)/DB選択.vb
+++ 予算管理システム(仮)/DB選択.vb
@@ -8,20 +8,25 @@ Imports C1.Win.C1FlexGrid
 Public Class DB選択
 
     Private Sub DB選択_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            FileList.Rows(0).Height = 20
 
-        FileList.Rows(0).Height = 20
+            Dim DataCount As Integer = 1
 
-        Dim DataCount As Integer = 1
-
-        ホーム.SystemMdf.CommandText = "SELECT TOP 5 * FROM userfiles ORDER BY filedate DESC"
-        Dim FileReader As SqlDataReader = ホーム.SystemMdf.ExecuteReader
-        While FileReader.Read
-            FileList(DataCount, 1) = FileReader.Item("filename")
-            FileList(DataCount, 2) = FileReader.Item("filepath")
-            DataCount += 1
-        End While
-        FileReader.Close()
-
+            ホーム.SystemMdf.CommandText = "SELECT TOP 5 * FROM userfiles ORDER BY filedate DESC"
+            Dim FileReader As SqlDataReader = ホーム.SystemMdf.ExecuteReader
+            While FileReader.Read
+                FileList(DataCount, 1) = FileReader.Item("filename")
+                FileList(DataCount, 2) = FileReader.Item("filepath")
+                DataCount += 1
+            End While
+            FileReader.Close()
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
     End Sub
 
     Private Sub Open_MouseEnter(sender As Object, e As EventArgs) Handles Open.MouseEnter
@@ -63,111 +68,123 @@ Public Class DB選択
     End Sub
 
     Private Sub OtherFileOpenDialog_FileOk(sender As Object, e As CancelEventArgs) Handles OtherFileOpenDialog.FileOk
+        Try
+            Dim OpenFileName As String = IO.Path.GetFileName(OtherFileOpenDialog.FileName)
+            Dim OpenFilePath As String = IO.Path.GetDirectoryName(OtherFileOpenDialog.FileName)
 
-        Dim OpenFileName As String = IO.Path.GetFileName(OtherFileOpenDialog.FileName)
-        Dim OpenFilePath As String = IO.Path.GetDirectoryName(OtherFileOpenDialog.FileName)
+            If OpenFileName.Contains(".mdf") = False Then
+                MsgBox("mdfファイルを選択してください。", MsgBoxStyle.Exclamation, "エラー")
+                Exit Sub
 
-        If OpenFileName.Contains(".mdf") = False Then
-            MsgBox("mdfファイルを選択してください。", MsgBoxStyle.Exclamation, "エラー")
-            Exit Sub
+            Else
 
-        Else
+                ホーム.SystemMdf.CommandText = ""
+                ホーム.SystemMdf.Parameters.Clear()
 
-            ホーム.SystemMdf.CommandText = ""
+                ホーム.SystemMdf.CommandText = "SELECT Count(*) FROM userfiles WHERE filename=@filename"
+                ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filename", SqlDbType.NVarChar))
+                ホーム.SystemMdf.Parameters("@filename").Value = OpenFileName
+                Dim Filecount As Integer = ホーム.SystemMdf.ExecuteScalar
+
+                If Filecount >= 1 Then
+                    ホーム.SystemMdf.CommandText = "UPDATE userfiles SET filedate='" & Now & "' WHERE filename=@filename"
+                    ホーム.SystemMdf.ExecuteNonQuery()
+                Else
+                    ホーム.SystemMdf.CommandText = "INSERT INTO userfiles (filename,filepath,filedate) VALUES (@filename,@filepath,@filedate)"
+                    ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filepath", SqlDbType.NVarChar))
+                    ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filedate", SqlDbType.DateTime))
+                    ホーム.SystemMdf.Parameters("@filepath").Value = OpenFilePath
+                    ホーム.SystemMdf.Parameters("@filedate").Value = Now
+                    ホーム.SystemMdf.ExecuteNonQuery()
+                End If
+            End If
+
             ホーム.SystemMdf.Parameters.Clear()
 
-            ホーム.SystemMdf.CommandText = "SELECT Count(*) FROM userfiles WHERE filename=@filename"
-            ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filename", SqlDbType.NVarChar))
-            ホーム.SystemMdf.Parameters("@filename").Value = OpenFileName
-            Dim Filecount As Integer = ホーム.SystemMdf.ExecuteScalar
+            ホーム.Connection.Close()
+            ホーム.Connection.Dispose()
 
-            If Filecount >= 1 Then
-                ホーム.SystemMdf.CommandText = "UPDATE userfiles SET filedate='" & Now & "' WHERE filename=@filename"
-                ホーム.SystemMdf.ExecuteNonQuery()
+            ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & OtherFileOpenDialog.FileName & ";Integrated Security=True"
+            ホーム.Connection.Open()
+            ホーム.Sql.Connection = ホーム.Connection
+            ホーム.Text = "予算管理システム　(" & OpenFilePath & "\" & OpenFileName & ")"
+
+
+            Me.Close()
+
+            ホーム.Sql.CommandText = "SELECT Count(budget_no) FROM budget_summary"
+            Dim DataCount As Integer = ホーム.Sql.ExecuteScalar
+
+
+            If DataCount >= 1 Then
+                予算選択.Show()
+
+                予算選択.TopMost = True
+                予算選択.TopMost = False
+
             Else
-                ホーム.SystemMdf.CommandText = "INSERT INTO userfiles (filename,filepath,filedate) VALUES (@filename,@filepath,@filedate)"
-                ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filepath", SqlDbType.NVarChar))
-                ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filedate", SqlDbType.DateTime))
-                ホーム.SystemMdf.Parameters("@filepath").Value = OpenFilePath
-                ホーム.SystemMdf.Parameters("@filedate").Value = Now
-                ホーム.SystemMdf.ExecuteNonQuery()
+                ホーム.Enabled = True
+                ホーム.BudgetNo = 0
             End If
-        End If
 
-
-
-        ホーム.Connection.Close()
-        ホーム.Connection.Dispose()
-
-        ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & OtherFileOpenDialog.FileName & ";Integrated Security=True"
-        ホーム.Connection.Open()
-        ホーム.Sql.Connection = ホーム.Connection
-        ホーム.Text = "予算管理システム　(" & OpenFilePath & "\" & OpenFileName & ")"
-
-
-        Me.Close()
-
-        ホーム.Sql.CommandText = "SELECT Count(budget_no) FROM budget_summary"
-        Dim DataCount As Integer = ホーム.Sql.ExecuteScalar
-
-
-        If DataCount >= 1 Then
-            予算選択.Show()
-
-            予算選択.TopMost = True
-            予算選択.TopMost = False
-
-        Else
-            ホーム.Enabled = True
-            ホーム.BudgetNo = 0
-        End If
-
-
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
 
     End Sub
 
     Private Sub Open_Click(sender As Object, e As EventArgs) Handles Open.Click
-
-        Dim SelectRow As Integer = FileList.Row
-        Dim OpenFileName As CellRange = FileList.GetCellRange(SelectRow, 1)
-        Dim OpenFilePath As CellRange = FileList.GetCellRange(SelectRow, 2)
-
-
-        ホーム.SystemMdf.CommandText = ""
-        ホーム.SystemMdf.Parameters.Clear()
-        ホーム.SystemMdf.CommandText = "UPDATE userfiles SET filedate=@filedate WHERE filename=@filename"
-        ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filedate", SqlDbType.DateTime))
-        ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filename", SqlDbType.NVarChar))
-        ホーム.SystemMdf.Parameters("@filedate").Value = Now
-        ホーム.SystemMdf.Parameters("@filename").Value = OpenFileName.Data
-        ホーム.SystemMdf.ExecuteNonQuery()
-
-        ホーム.Connection.Close()
-        ホーム.Connection.Dispose()
-
-        ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & OpenFilePath.Data & OpenFileName.Data & ";Integrated Security=True"
-        ホーム.Connection.Open()
-        ホーム.Sql.Connection = ホーム.Connection
-        ホーム.Text = "予算管理システム　(" & OpenFilePath.Data & "\" & OpenFileName.Data & ")"
+        Try
+            Dim SelectRow As Integer = FileList.Row
+            Dim OpenFileName As CellRange = FileList.GetCellRange(SelectRow, 1)
+            Dim OpenFilePath As CellRange = FileList.GetCellRange(SelectRow, 2)
 
 
-        ホーム.Sql.CommandText = "SELECT Count(budget_no) FROM budget_summary"
-        Dim DataCount As Integer = ホーム.Sql.ExecuteScalar
 
-        Me.Close()
+            ホーム.SystemMdf.CommandText = ""
+            ホーム.SystemMdf.Parameters.Clear()
+            ホーム.SystemMdf.CommandText = "UPDATE userfiles SET filedate=@filedate WHERE filename=@filename"
+            ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filedate", SqlDbType.DateTime))
+            ホーム.SystemMdf.Parameters.Add(New SqlParameter("@filename", SqlDbType.NVarChar))
+            ホーム.SystemMdf.Parameters("@filedate").Value = Now
+            ホーム.SystemMdf.Parameters("@filename").Value = OpenFileName.Data
+            ホーム.SystemMdf.ExecuteNonQuery()
 
-        If DataCount >= 1 Then
-            予算選択.Show()
+            ホーム.Connection.Close()
+            ホーム.Connection.Dispose()
 
-            予算選択.TopMost = True
-            予算選択.TopMost = False
+            ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & OpenFilePath.Data & OpenFileName.Data & ";Integrated Security=True"
+            ホーム.Connection.Open()
+            ホーム.Sql.Connection = ホーム.Connection
+            ホーム.Text = "予算管理システム　(" & OpenFilePath.Data & "\" & OpenFileName.Data & ")"
+            ホーム.UserDataPath = OpenFilePath.Data
+            ホーム.UserDataName = OpenFileName.Data
 
-        Else
-            ホーム.Enabled = True
-            ホーム.BudgetNo = 0
-        End If
+            ホーム.Sql.CommandText = "SELECT Count(budget_no) FROM budget_summary"
+            Dim DataCount As Integer = ホーム.Sql.ExecuteScalar
 
+            Me.Close()
 
+            If DataCount >= 1 Then
+                予算選択.Show()
+
+                予算選択.TopMost = True
+                予算選択.TopMost = False
+
+            Else
+                ホーム.Enabled = True
+                ホーム.BudgetNo = 0
+            End If
+
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
 
     End Sub
 End Class
--- 予算管理システム(仮)/予算選択.vb
+++ 予算管理システム(仮)/予算選択.vb
@@ -6,72 +6,87 @@ Imports System.Deployment.Application.ApplicationDeployment
 Imports System.Windows.Forms.Form
 Public Class 予算選択
     Private Sub OK_Click(sender As Object, e As EventArgs) Handles OK.Click
-        If ホーム.Connection.State = ConnectionState.Open Then
-            ホーム.Connection.Close()
-        End If
+        Try
+            If ホーム.Connection.State = ConnectionState.Open Then
+                ホーム.Connection.Close()
+            End If
 
-        ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & FilePath.Text & ";Integrated Security=True"
-        ホーム.Connection.Open()
-        ホーム.Sql.Connection = ホーム.Connection
+            ホーム.Connection.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & FilePath.Text & ";Integrated Security=True"
+            ホーム.Connection.Open()
+            ホーム.Sql.Connection = ホーム.Connection
 
-        ホーム.Enabled = True
+            ホーム.Enabled = True
 
-        If Contract_NoList.SelectedItem = "当初" Then
-            ホーム.Text = ホーム.Text & "(当初)" & FilePath.Text
-            ホーム.BudgetNo = 0
-        ElseIf Contract_NoList.SelectedItem = "変更予算作成" Then
+            If Contract_NoList.SelectedItem = "当初" Then
+                ホーム.Text = "予算管理システム(当初)" & FilePath.Text
+                ホーム.BudgetNo = 0
+            ElseIf Contract_NoList.SelectedItem = "変更予算作成" Then
 
-        Else
-            ホーム.Text = ホーム.Text & "(" & Contract_NoList.SelectedItem & ")" & FilePath.Text
-            Dim BudgetNo As String = Contract_NoList.SelectedItem
-            BudgetNo = BudgetNo.Replace("第", "")
-            BudgetNo = BudgetNo.Replace("回変更", "")
-            ホーム.BudgetNo = BudgetNo
-        End If
+            Else
+                ホーム.Text = "予算管理システム" & "(" & Contract_NoList.SelectedItem & ")" & FilePath.Text
+                Dim BudgetNo As String = Contract_NoList.SelectedItem
+                BudgetNo = BudgetNo.Replace("第", "")
+                BudgetNo = BudgetNo.Replace("回変更", "")
+                ホーム.BudgetNo = BudgetNo
+            End If
 
-        Me.Close()
+            Me.Close()
+
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
     End Sub
 
     Private Sub 予算選択_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
 
-        If ホーム.UserDataName <> "" Then
+            If ホーム.UserDataName <> "" Then
 
-            Dim Prjct_Name As String
-            Dim Prjct_Code As String
-            Dim budget_no As Integer = 0
+                Dim Prjct_Name As String
+                Dim Prjct_Code As String
+                Dim budget_no As Integer = 0
 
-            ホーム.Sql.CommandText = "select max(budget_no) from budget_summary"
-            ホーム.BudgetNo = ホーム.Sql.ExecuteScalar
+                ホーム.Sql.CommandText = "select max(budget_no) from budget_summary"
+                ホーム.BudgetNo = ホーム.Sql.ExecuteScalar
 
-            ホーム.Sql.CommandText = "SELECT contents FROM controldata WHERE class_code=20"
-            Prjct_Code = ホーム.Sql.ExecuteScalar
+                ホーム.Sql.CommandText = "SELECT contents FROM controldata WHERE class_code=20"
+                Prjct_Code = ホーム.Sql.ExecuteScalar
 
-            ホーム.Sql.CommandText = "SELECT contents FROM controldata WHERE class_code=21"
-            Prjct_Name = ホーム.Sql.ExecuteScalar
+                ホーム.Sql.CommandText = "SELECT contents FROM controldata WHERE class_code=21"
+                Prjct_Name = ホーム.Sql.ExecuteScalar
 
-            Contract_NoList.Items.Add("当初")
+                Contract_NoList.Items.Add("当初")
 
-            For No As Integer = 1 To budget_no
-                Contract_NoList.Items.Add("第" & No & "回変更")
-            Next
+                For No As Integer = 1 To budget_no
+                    Contract_NoList.Items.Add("第" & No & "回変更")
+                Next
 
-            Contract_NoList.Items.Add("変更予算作成")
+                Contract_NoList.Items.Add("変更予算作成")
 
-            If ホーム.BudgetNo = 0 Then
-                Contract_NoList.SelectedItem = "当初"
+                If ホーム.BudgetNo = 0 Then
+                    Contract_NoList.SelectedItem = "当初"
+                Else
+                    Contract_NoList.SelectedItem = "第" & ホーム.BudgetNo & "回変更"
+                End If
+
+                Project.Value = Prjct_Code & " " & Prjct_Name
+                FilePath.Value = ホーム.UserDataPath & ホーム.UserDataName
+
             Else
-                Contract_NoList.SelectedItem = "第" & ホーム.BudgetNo & "回変更"
+
+                ホーム.Enabled = True
+                Me.Close()
             End If
 
-            Project.Value = Prjct_Code & " " & Prjct_Name
-            FilePath.Value = ホーム.UserDataPath & ホーム.UserDataName
-
-        Else
-
-            ホーム.Enabled = True
-            Me.Close()
-        End If
-
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
     End Sub
     Private Sub OK_MouseEnter(sender As Object, e As EventArgs) Handles OK.MouseEnter
         OK.Cursor = Cursors.Hand
