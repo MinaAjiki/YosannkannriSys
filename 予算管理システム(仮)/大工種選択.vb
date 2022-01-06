40b7961b5b44c5f0e8f55d0d9e10946b25ae2000
MinaAjiki <mina_ajiki@ic-group.co.jp>
2022-01-06 09:15:29 +0900

    修正

--- 予算管理システム(仮)/大工種選択.vb
+++ 予算管理システム(仮)/大工種選択.vb
@@ -3,47 +3,49 @@ Imports System.Data.SqlClient
 Imports System.Windows.Forms.Form
 Public Class 大工種選択
     Private Sub 大工種選択_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Try
-        Dim Total As Int64 = 0
+        Try
+            Dim Total As Int64 = 0
 
             ホーム.SystemSql.CommandText = "SELECT  Count(*) FROM l_worktypes"
             Dim Count As Integer = ホーム.SystemSql.ExecuteScalar
 
-        L_WorkTypesList.Rows.Count = Count + 1
+            L_WorkTypesList.Rows.Count = Count + 1
 
-        ホーム.Sql.Parameters.Clear()
-        ホーム.Sql.CommandText = ""
+            ホーム.Sql.Parameters.Clear()
+            ホーム.Sql.CommandText = ""
 
-        Dim RowCount As Integer = 1
-        ホーム.Sql.CommandText = "SELECT * FROM L_worktype_total WHERE budget_no=" & ホーム.BudgetNo & " OR budget_no IS NULL ORDER BY s_wrktyp_code ASC"
-        L_WorkTypesList(RowCount, 3) = ホーム.Sql.ExecuteScalar
-        Dim LWorkTypeReader As SqlDataReader = ホーム.SystemSql.ExecuteReader
-        While LWorkTypeReader.Read
+            Dim RowCount As Integer = 1
+            ホーム.SystemSql.CommandText = "SELECT  * FROM l_worktypes ORDER BY l_wrktyp_code ASC"
+            Dim LWorkTypeReader As SqlDataReader = ホーム.SystemSql.ExecuteReader
+            While LWorkTypeReader.Read
 
                 L_WorkTypesList(RowCount, 1) = LWorkTypeReader.Item("l_wrktyp_code")
-            L_WorkTypesList(RowCount, 2) = LWorkTypeReader.Item("l_wrktyp_name")
+                L_WorkTypesList(RowCount, 2) = LWorkTypeReader.Item("l_wrktyp_name")
 
+                ホーム.Sql.CommandText = "SELECT Count(*) FROM details WHERE budget_no=" & ホーム.BudgetNo
+                Dim DetailsCount As Integer = ホーム.Sql.ExecuteScalar
 
+                If DetailsCount > 0 Then
+                    ホーム.Sql.CommandText = "SELECT SUM(amout_total) FROM L_worktype_total WHERE budget_no=" & ホーム.BudgetNo & " AND l_wrktyp_code=" _
+                                                    & LWorkTypeReader.Item("l_wrktyp_code")
+                    L_WorkTypesList(RowCount, 3) = ホーム.Sql.ExecuteScalar
 
+                    Total += L_WorkTypesList(RowCount, 3)
 
-            ホーム.Sql.CommandText = "SELECT SUM(amount_total) FROM L_worktype_total WHERE budget_no=" & ホーム.BudgetNo & " AND l_wrktyp_code=" _
-                                                & LWorkTypeReader.Item("l_wrktyp_code")
-                L_WorkTypesList(RowCount, 3) = ホーム.Sql.ExecuteScalar
-
-                Total += L_WorkTypesList(RowCount, 3)
-
+                End If
                 RowCount += 1
+
             End While
             LWorkTypeReader.Close()
 
             LWorktypeTotal.Text = Total
 
-        'Catch ex As Exception
-        '    ホーム.ErrorMessage = ex.Message
-        '    ホーム.StackTrace = ex.StackTrace
-        '    エラー.Show()
-        '    Exit Sub
-        'End Try
+        Catch ex As Exception
+            ホーム.ErrorMessage = ex.Message
+            ホーム.StackTrace = ex.StackTrace
+            エラー.Show()
+            Exit Sub
+        End Try
     End Sub
 
     Private Sub Cancel_MouseDown(sender As Object, e As MouseEventArgs) Handles Cancel.MouseDown
