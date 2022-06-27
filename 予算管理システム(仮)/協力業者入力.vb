f30c8c1ba11db3f4213a357fdd5ccffd0fc4f0b3
217003 <kazumasa_suzuki@ic-group.co.jp>
2022-06-27 16:07:32 +0900

    協力業者入力

--- 予算管理システム(仮)/協力業者入力.vb
+++ 予算管理システム(仮)/協力業者入力.vb
@@ -227,7 +227,9 @@ Public Class 協力業者入力
                     ホーム.Sql.Parameters("@outsrcr_term_s").Value = Coopterms.Data
                     ホーム.Sql.Parameters("@outsrcr_term_e").Value = Coopterme.Data
 
-                    ホーム.SystemSql.CommandText = "SELECT detail_code FROM name_masters WHERE class_code = 4 AND item_name = " & " & Coopordr.Data & "
+                    ホーム.SystemSql.Parameters.Clear()
+                    ホーム.SystemSql.CommandText = "SELECT detail_code FROM name_masters WHERE class_code = 4 AND item_name = @Coopordr"
+                    ホーム.SystemSql.Parameters.Add(New SqlParameter("@Coopordr", SqlDbType.NVarChar)).Value = Coopordr.Data
                     Dim dtlcode As String = ホーム.SystemSql.ExecuteScalar
                     ホーム.Sql.Parameters("@ordrfrm").Value = dtlcode
                     'If Coopordr.Data = "工事課発注" Then
